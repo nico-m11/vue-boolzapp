f25bf825c31263158f37780c897f53c5cd2a3711
Nicola <nicolamelito.11@gmail.com>
2020-11-22 18:58:59 +0100

    Milestone 1
    devo rivedere solo il responsive

---
+++ js/script.js
@@ -0,0 +1,18 @@
+const app = new Vue ({
+  el: '#root',
+  data: {
+    //myArray: [{
+    //  src:"img/avatar_io.jpg",
+    //  nome: 'Mery'
+  //  },
+  //  {
+  //    scr:"img/avatar_3.jpg",
+  //    nome: "Ernesto"
+  //  },
+//    {
+//      scr: "img/avatar_4.jpg",
+//      nome: "Genny"
+//    },
+//  ],
+},
+});
