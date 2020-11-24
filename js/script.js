34611c293a3d752b1083fa0a35e5ae7c135a22f5
Nicola <nicolamelito.11@gmail.com>
2020-11-24 16:08:00 +0100

    Milestone 3
    completata , da rivedere sempre il responsive

--- js/script.js
+++ js/script.js
@@ -7,12 +7,12 @@ const app = new Vue ({
         avatar: 'img/avatar_4.jpg',
         message: [
           {
-            textMessage:"Ue Fratm tutt appost?",
+            textMessage:"Ciao Genny come stai?",
             date:'23/11/2020 23:00',
             type: 'sent'
           },
           {
-            textMessage:"Bro, tutt appost, semp e solit cos",
+            textMessage:"Ciao Nico sto una favola e tu?",
             date:"23/11/2020 23:05",
             type: 'recived'
           },
@@ -23,12 +23,12 @@ const app = new Vue ({
         avatar: 'img/avatar_io.jpg',
         message: [
           {
-            textMessage: 'Biò dove stai?',
+            textMessage: 'Hey non ti fai vedere più, che fine hai fatto?',
             date: '22/11/2020 15:00',
             type: 'sent'
           },
           {
-            textMessage:'sto sul corso di Secondigliano tu?',
+            textMessage:'sono impegnata con il lavoro',
             date: '22/11/2020 15:15',
             type: 'recived'
           },
@@ -39,23 +39,56 @@ const app = new Vue ({
         avatar: 'img/avatar_3.jpg',
         message: [
           {
-            textMessage: 'Ue Fratm o Casertà',
+            textMessage: 'Quando ci prendiamo un Drink',
             date: '18/11/2020 00:00',
             type: 'sent'
           },
           {
-            textMessage:'Ue Fratm o Napulign',
+            textMessage:'anche adesso!',
             date: '18/11/2020 04:55',
+            type: 'recived'
+          },
+        ]
+      },
+      {
+        nome: 'Antonio Concilio',
+        avatar: 'img/avatar_7.jpg',
+        message: [
+          {
+            textMessage: "Quando posso portare l'auto a lavare?",
+            date: '18/11/2020 12:00',
+            type: 'sent'
+          },
+          {
+            textMessage:'vieni domani che sono più libero',
+            date: '18/11/2020 12:15',
             type: 'recived'
           },
         ]
       },
     ],
     activeContact: 0,
+    textInput: ''
 },
   methods: {
     setActiveContact: function (index) {
       this.activeContact = index;
+    },
+    sent: function () {
+      this.contacts[this.activeContact].message.push({
+        textMessage: this.textInput,
+        type: 'sent',
+        date: '24/11/2020 15:00'
+      })
+      this.textInput= '';
+      setTimeout(this.recived, 5000);
+    },
+    recived: function () {
+      this.contacts[this.activeContact].message.push({
+        textMessage: 'Perfetto',
+        type: 'recived',
+        date: '24/11/2020 15:05'
+      })
     }
   }
 });
